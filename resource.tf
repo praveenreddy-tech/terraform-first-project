d83feecd86b85ce6f81f2f0906d2e7d09346da97
unknown <labuser@praveen.com>
2025-07-16 09:28:00 +0000

    second

--- resource.tf
+++ resource.tf
@@ -1,6 +1,34 @@
-provider "azurerm" {
-    features {}
-}resource "azurerm_resource_group" "terra" {
-  name     = "terra"
-  location = "East US"
+
+
+resource "azurerm_resource_group" "storeterra" {
+
+    name     = "example-resources"
+
+    location = "East US"
+
 }
+ 
+resource "azurerm_storage_account" "saterra" {
+
+    name                     = "newstorageaccount"
+
+    resource_group_name      = azurerm_resource_group.storeterra.name
+
+    location                 = azurerm_resource_group.storeterra.location
+
+    account_tier             = "Standard"
+
+    account_replication_type = "LRS"
+
+}
+ 
+resource "azurerm_storage_container" "sacontainer" {
+
+    name                  = "terra-container"
+
+    storage_account_name  = azurerm_storage_account.saterra.name
+
+    container_access_type = "private"
+
+}
+ 
